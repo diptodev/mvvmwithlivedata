57bc6f1caf77c736619bba74735700672f121ccc
diptodev <dipankardebnathcse@yahoo.com>
2023-05-30 10:28:13 +0600

    data binding dependancy added

--- app/src/main/java/com/excitedbroltd/mvvmwithlivedata/MainActivity.kt
+++ app/src/main/java/com/excitedbroltd/mvvmwithlivedata/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.excitedbroltd.mvvmwithlivedata
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.excitedbroltd.mvvmwithlivedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 }
