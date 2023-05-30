f2ed151d104e7944c955c4e68f23b3c6dd8702ef
diptodev <dipankardebnathcse@yahoo.com>
2023-05-31 00:11:01 +0600

    edit some extra code

--- app/src/main/java/com/excitedbroltd/mvvmwithlivedata/MyViewModel.kt
+++ app/src/main/java/com/excitedbroltd/mvvmwithlivedata/MyViewModel.kt
@@ -8,10 +8,13 @@ class MyViewModel(startingTotal: Int) : ViewModel() {
 
     var count = MutableLiveData<Int>()
     val totalCount: LiveData<Int> get() = count
+    var userName = MutableLiveData<String>()
 
     init {
         count.value = startingTotal
+        userName.value = "Dipto"
     }
+
 
     fun setValue() {
         count.value = (totalCount.value)?.plus(1)
